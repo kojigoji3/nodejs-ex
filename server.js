3210328aaf85f65539cdcb2233f68ce3a3cf39b8
kojima <kojimah3@gmail.com>
2017-08-18 10:48:28 +0900

    Add Clock function

--- server.js
+++ server.js
@@ -91,6 +91,27 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+
+//////////addtest
+app.get('/time', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('clock.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('clock.html', { pageCountMessage : null});
+  }
+});
+////////////
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
