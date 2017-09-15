b00ce020bb39250d857d9e01d0ae569af92c4a19
kojima <kojimah3@gmail.com>
2017-09-15 19:46:49 +0900

    データ受け取り拡張

--- server.js
+++ server.js
@@ -69,8 +69,7 @@ var initDb = function(callback) {
 
 
 
-//-IF----------------------
-
+//-original----------------------
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -80,7 +79,7 @@ app.get('/', function (req, res) {
   if (db) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
+    //col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
@@ -105,7 +104,8 @@ app.get('/pagecount', function (req, res) {
 });
 
 
-//////////addtest
+
+//-ADD-TEST----------------------
 app.get('/time', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -135,41 +135,107 @@ app.post('/test_post', function (req, res) {
 });
 
 
-app.post('/test_dbsave', function (req, res) {
+//-ADD-IF----------------------
+app.post('/post_sensor_data', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
+  if (!db) { initDb(function(err){}); }
   
   if (db && req.body != null && req.body != "" && req.body != {} && req.body != []) {
-    var col = db.collection('datas');
+
+    var dt = new Date();
+    var yy = dt.getFullYear();
+    var mm = dt.getMonth();
+    var dd = dt.getDate();
+    var hh = dt.getHours();
+    var mi = dt.getMinutes();
+    var ss = dt.getSeconds();
+    req.body['year'] = Number(yy);
+    req.body['month'] = Number(mm)+1; 
+    req.body['day'] =  Number(dd);
+    req.body['hour'] = Number(hh);
+    req.body['minute'] = Number(mi);
+    req.body['second'] = Number(ss);
+    console.log(req.body);
+
+    var col = db.collection('sensor_datas');
     col.insert(req.body);
     col.count(function(err, count){
-      res.send('{ datas_count: ' + count + '}');
+      res.status(200).send('{ datas_count: ' + count + '}');
     });
   } else {
-    res.send('{ add_count: 0}');
+    res.status(500).send('{ add_count: 0}');
   }
 });
 
-app.get('/test_dbget', function (req, res) {
+app.get('/get_sensor_data', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  
-  if (db) {
-    var col = db.collection('datas');
-    var ary = col.find().toArray();
-    res.send(ary);
+  if (!db) { initDb(function(err){}); }
 
-  } else {
-    res.send('{ get_count: 0}');
+  var datas = [];
+  var getyear = req.query.year;
+  var getmonth = req.query.month;
+  var getday = req.query.day;
+  var gettime = req.query.hour;
+  var yy1 = Number(getyear);
+  var mm1 = Number(getmonth);//1-12
+  var mm1b = mm1 - 1;//0-11
+  var dd1 = Number(getday);
+  var hh1 = Number(gettime);
+
+  if (db && getyear != undefined && getyear != '' && getmonth != undefined && getmonth != ''
+         && getday != undefined  && getday != ''  && gettime != undefined  && gettime != '' 
+         && 0 < yy1 && 0 < mm1 && mm1 <= 12 && 0 < dd1 && dd1 <= 31 && 0 < hh1 && hh1 <= 24) {
+         
+    var yesterdate = new Date(yy1, mm1b, dd1, hh1, 0, 0);
+    yesterdate.setDate(yesterdate.getDate()-1);
+    
+    var yy2 = yesterdate.getFullYear();
+    var mm2b = yesterdate.getMonth();//0-11
+    var mm2 = mm2b+1;//1-12
+    var dd2 = yesterdate.getDate();
+    var hh2 = yesterdate.getHours();
+         
+         
+    var col = db.collection('sensor_datas');
+    var getquery = { $or : [ 
+                            {year : yy2, month : mm2, day : dd2, hour: { $gte: hh2 } }, 
+                            {year : yy1, month : mm1, day : dd1, hour: { $lte: hh1 } } 
+                           ]
+                   };
+    console.log(getquery);
+    var ary = col.find(getquery).toArray((error, documents) => {
+      for(var doc of documents){
+        datas.push({'id' : doc.id, 'temperature' : doc.temperature, 'humidity' : doc.humidity, 'pressure' : doc.pressure,
+                    'year' : doc.year,  'month' : doc.month,  'day' : doc.day,  
+                    'hour': doc.hour, 'minute' : doc.minute, 'second' : doc.second  });
+      }
+      console.log('OK');
+      console.log(documents);
+      res.status(200).json(datas);
+    });
+  } 
+  else {
+    res.status(500).send("QueryError year=*&month=*&day=*&hour=*");
   }
 });
 
+
+app.get('/delete_sensor_datas', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) { initDb(function(err){}); }
+
+  if (db) {
+    var col = db.collection('sensor_datas');
+    var ary = col.deleteMany();
+    res.status(200).send('Delete Success');
+  } 
+  else {
+    res.status(500).send('Delete Error');
+  }
+});
 
 ////////////
 
